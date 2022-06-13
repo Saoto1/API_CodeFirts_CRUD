7cf5a4ac3846939ad1a4c493cfbd13335d7e98f9
Saul Valdez Vasquez <sv610683@digicelgroup.com>
2022-06-12 21:51:44 -0600

    Metodos Crud de prueba, entidad cliente

---
+++ API_CodeFirts_CRUD/Controllers/ValuesController.cs
@@ -0,0 +1,85 @@
+﻿using DB_Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API_CodeFirts_CRUD.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private DBContext _context;
+
+        public ValuesController( DBContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<Clientes> Get()
+        {
+          return  _context.Clientes.ToList();           
+        }   
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public IEnumerable<Clientes> Get(int id)
+        {
+            var ReturID = from n in _context.Clientes where n.Id.Equals(id) select n;
+            return ReturID.ToList();
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] Clientes cliente )
+        {
+            try
+            {
+                _context.Add(cliente);
+                await _context.SaveChangesAsync();
+                return Ok();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+                
+            }
+
+            
+
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Clientes cliente)
+        {
+            try
+            {
+                var clientes = _context.Clientes.FirstOrDefault(s=> s.Id == id);
+                clientes.Nombre = cliente.Nombre;
+                clientes.Apellido = cliente.Apellido;
+                clientes.DUI = cliente.DUI;
+                clientes.NumeroTel = cliente.NumeroTel;
+                _context.Update(clientes);
+                _context.SaveChanges();
+
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- API_CodeFirts_CRUD/Program.cs
+++ API_CodeFirts_CRUD/Program.cs
@@ -11,22 +11,23 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 //Conexion a la DB
-//builder.Services.AddDbContext<DBContext>(options => {
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("BibliotecaConnection"));
+builder.Services.AddDbContext<DBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BibliotecaConnection"));
 
-//});
+});
 
 
 var app = builder.Build();
 
 //Inicia una migracion, para la creacion de lka base de datos, esto se ejecuta,cada vez
 //que se inicia el proyecto.
-using (var scope = app.Services.CreateScope())
-{ 
-    var context = scope.ServiceProvider.GetRequiredService<DBContext>();
-    context.Database.Migrate();
+//using (var scope = app.Services.CreateScope())
+//{ 
+//    var context = scope.ServiceProvider.GetRequiredService<DBContext>();
+//    context.Database.Migrate();
 
-}
+//}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
